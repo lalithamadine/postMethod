5c684f6a8f609790442812ef17a01a4879b82237
lalithamadine <45647968+lalithamadine@users.noreply.github.com>
2018-12-26 15:37:27 +0530

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    axios.post('http://checklister-api.herokuapp.com/item/create/ ',
+      { headers: {'Content-Type': 'application/json'},
+        'name': 'gfds',
+        'description': '',
+        'description_html': ',',
+        'details': {
+          'address': '',
+          'gender': '',
+          'mobile': ';'
+        },
+        'is_complete': 'true',
+        'priority': 'H',
+        'user': '1'
+      })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+  }
+}
+</script>
